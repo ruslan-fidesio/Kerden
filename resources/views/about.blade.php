372bf56b7865907cff14a8cced4a30e798d61df0
PierreWack <pierre.wack@gmail.com>
2017-03-27 00:38:01 +0200

    about text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -47,34 +47,46 @@
 <!--       <div class="col-sm-4">
         <img class='img-responsive' src="{{asset('/images/team/alex.png')}}" alt="alexandre">    
       </div> -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">ALEXANDRE</h3>          
         <h4 class="team-subtitle">Inventif, créatif, sportif.</h4>
         <p class='text-justify'>Etudiant en Architecture d’intérieur à l’école Penninghen, Alexandre a pris conscience du lien entre ; La création, l’art, et la nature. Il a eu alors l’idée de relier son expérience à un concept plus universel, ouvert à tous, créant grandeur nature un nouvel art de vivre: Un site de location de jardins.<br/>Alexandre aime l’eau des ruisseaux en perpétuel mouvement, se frayant ainsi un chemin à travers les éléments.</p>
-      </div>
+      </div>   
 <!--       <div class="col-sm-4">
         <img style="transform:scale(0.8)" class='img-responsive' src="{{asset('/images/team/alice.png')}}" alt="alice">
       </div> -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">ALICE</h3>
         <h4 class="team-subtitle">Energique, créative, cinéphile.</h4>
         <p class='text-justify'>Etudiante en école de cinéma, et élève des cours Florent, Alice a réalisé avec ses amis la vidéo du site, et est en charge de la communication par l’image : Photo et vidéo.<br/>Sa participation a pour objectif de faire vivre le site et d’inspirer les utilisateurs. Alice est passionnée depuis ses jeunes années par la photographie et le cinéma ; Ses réalisations ont d’abord été le témoin de ses aventures, puis sont devenus un support de développement créatif.
         </p>
       </div>
+
 <!--       <div class="col-sm-4">
         <img style="transform:scale(0.8)" class='img-responsive' src="{{asset('/images/team/camille.png')}}" alt="camille">
       </div> -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">CAMILLE</h3>
         <h4 class="team-subtitle">Voyageuse, créative, brillante.</h4>
         <p class='text-justify'>Diplômée d’une école de Commerce, Camille a voyagé pendant un an en Australie et Nouvelle-Zélande. Elle est responsable du développement marketing et du suivi comptable du site. Camille aime le voyage car il favorise l’ouverture et la richesse intérieure. Elle est également passionnée par l’univers sportif.</p>
       </div>
+<!--       <div class="col-sm-4">
+        <img style="transform:scale(0.8)" class='img-responsive' src="{{asset('/images/team/alice.png')}}" alt="alice">
+      </div> -->
+      <div class="col-sm-3">
+        <h3 class="team-title">JEAN-JACQUES</h3>
+        <h4 class="team-subtitle">Pragmatique, sociable, ouvert</h4>
+        <p class='text-justify'>Etudiant en école de commerce et magicien autodidacte, Jean-Jacques porte une grande importance aux relations humaines.<br>
+        Responsable commerciale et relation clients au sein de Kerden. il veille à ce que chaque utilisateur soit satisfait et trouve son bonheur au sein de l’expérience Kerden.
+        </p>
+      </div>       
     </div>
+
     <div class="row">
 <!--       <div class="col-sm-4">
         <img class='img-responsive' src="{{asset('/images/team/liza.png')}}" alt="liza">
       </div>   -->    
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">LIZA</h3>
         <h4 class="team-subtitle">Poète, artiste, lumineuse.</h4>
         <p class='text-justify'>Diplômée de L’Institut Supérieur des Arts Appliqués, Liza a mis ses talents d’illustratrice au service de KERDEN. Inspirée par la poésie de la nature et la douceur des pastels, elle transforme en dessins ses rêveries champêtres. Lorsqu’elle ne dessine pas, Liza aime sillonner les trottoirs parisiens à la recherche de nouvelles adresses secrètes ou s’adonner à ses autres passions, la décoration d'intérieur et la cuisine scandinave.<br><br></p>
@@ -82,7 +94,7 @@
 <!--       <div class="col-sm-4">
         <img class='img-responsive' src="{{asset('/images/team/simon.jpg')}}" alt="simon">
       </div> -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">SIMON</h3>
         <h4 class="team-subtitle">Naturel, Ingénieux, imaginatif.</h4>
         <p class='text-justify'>Développeur web, et passionné d'informatique, Simon a développé des sites pour de nombreuses entreprises. Lorsqu’il  a été mis en contact avec Alexandre par un ami commun, Simon a été emballé par son projet. Innovation et simplicité sont ses maîtres mots. Il aime la voile, la pêche, et la bonne cuisine.</p>
@@ -90,7 +102,7 @@
 <!--       <div class="col-sm-4">
         <img class='img-responsive' src="{{asset('/images/team/victoire.png')}}" alt="victoire">      
       </div> -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="team-title">VICTOIRE</h3>      
         <h4 class="team-subtitle">Philosophe, joyeuse, curieuse.</h4>
         <p class='text-justify'>Passionnée par la lecture dans toutes ses dimensions, de formation commerciale, ayant une expérience professionnelle variée, Victoire met des mots pour faire partager les concepts et les idées aux utilisateurs. Victoire aime les chansons, Saint-Exupéry, Einstein, Platon, Adrienne Farb, Antonio Canova, le Bocage Normand, et les repas arrosés...</p>
